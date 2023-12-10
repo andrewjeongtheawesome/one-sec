fd288a774a91f988e0e2ca2fe9d2342e02d3c89d
Jeong ji won <imprayer1@naver.com>
2023-12-10 22:54:08 +0900

    make frame

---
+++ index.js
@@ -0,0 +1,38 @@
+const startButton = document.querySelector("#startButton");
+const resetButton = document.querySelector("#resetButton")
+const endButton = document.querySelector("#endButton");
+const target = document.querySelector("#target");
+
+let isStartClicked = true;
+let isEndClicked = true;
+let isResetClicked = true;
+let start;
+let end;
+
+startButton.addEventListener("click", () => {
+    if(isStartClicked){
+        target.innerText = "wating for response...";
+        start = new Date();
+    }
+});
+
+endButton.addEventListener("click", () => {
+    if(isEndClicked){
+        end = new Date();
+        diff = end-start;
+        if(diff<1100 && 900<=diff){
+            target.innerText = `${diff} correct`;
+        }
+        else{
+            target.innerText = `${diff} incorrect`;
+        }
+        isEndClicked = true;
+    }
+});
+
+resetButton.addEventListener("click", () => {
+    if(isResetClicked){
+        target.innerText = "press start";
+        isResetClicked = true;
+    }
+});
